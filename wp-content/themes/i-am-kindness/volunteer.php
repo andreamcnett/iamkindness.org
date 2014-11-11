8edc0ada0492f0673e1ea5b8309d27e51000f0cb
Ben Demaree <demar136@umn.edu>
2014-11-10 20:11:48 -0600

    Add volunteer form.
    Also includes Ninja Forms installation.

--- wp-content/themes/i-am-kindness/volunteer.php
+++ wp-content/themes/i-am-kindness/volunteer.php
@@ -24,43 +24,7 @@ $introimage = get_field('intro-image');
 		</div>
 	</div>
 	<div class="page-copy">
-		<form class="pure-form pure-form-stacked">
-		    <fieldset>
-		        <div class="pure-control-group">
-		            <label for="name">Your Name</label>
-		            <input id="name" type="text" placeholder="Firstname Lastname">
-		        </div>
-		        <div class="pure-control-group">
-		            <label for="email">Email</label>
-		            <input id="email" type="email" placeholder="youremail@email.com">
-		        </div>
-		        <label for="hair" class="pure-checkbox">
-			        <input id="hair" type="checkbox" value="">
-			        Hair-styling
-			    </label>
-			    <label for="styling" class="pure-checkbox">
-			        <input id="styling" type="checkbox" value="">
-			        Fashion Styling
-			    </label>
-			    <label for="makeup" class="pure-checkbox">
-			        <input id="makeup" type="checkbox" value="">
-			        Make-up
-			    </label>
-			    <label for="kind" class="pure-checkbox">
-			        <input id="kind" type="checkbox" value="">
-			       Being Kind
-			    </label>
-			    <label for="other" class="pure-checkbox">
-			        <input id="other" type="checkbox" value="">
-			       Other
-			    </label>
-		        <div class="pure-control-group">
-		            <label for="something">Tell us anything about yourself</label>
-		            <input id="something" type="something" placeholder="180 characters or less, please">
-		        </div>
-		        <button type="submit" class="pure-button pure-button-primary">Submit</button>
-		    </fieldset>
-		</form>
+        <?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 2 ); } ?>
 	</div>
 </div>
 
